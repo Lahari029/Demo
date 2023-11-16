a469546b27108e89f317df3e10a4ca7c6e8245ca
lokeshnagineni98 <lokeshnagineni98@gmail.com>
2023-11-16 12:24:24 +0530

    eng1firstcommit

---
+++ src/test/resources/TestCases/tc2Test.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc2Test {
+	@Test
+	public void m2()
+	{
+	Reporter.log("tc2 running",true);	
+	}
+
+}
